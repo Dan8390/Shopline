c5a40b5bbbac40adb3cd23f19391df8627363d93
Daniel <daniel.byshovets@gmail.com>
2025-02-04 19:47:14 -0800

    Commit 9 (continued developing main moderator functions: finished show functions, started delete functions)

--- main/moderator/urls.py
+++ main/moderator/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.show_index, name="show_index"),
-    path('shop', views.show_shop, name="show_shop"),
-    path('users', views.show_users, name="show_users")
+    path('products', views.show_products, name="show_products"),
+    path('product_delete/<int:product_id>', views.show_product_delete, name="product_delete"),
+    path('users', views.show_users, name="show_users"),
+    path('user_delete/<int:user_id>', views.show_user_delete, name="user_delete"),
+    path('producers', views.show_producers, name="show_producers"),
+    path('producer_delete/<int:producer_id>', views.show_producer_delete, name="producer_delete")
 ]
--- main/moderator/views.py
+++ main/moderator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from user.models import User, Product, Producer
 
 # Create your views here.
 
@@ -7,9 +8,28 @@ def show_index(request):
     return render(request, "moderator/index.html")
 
 
-def show_shop(request):
-    return render(request, "moderator/products.html")
+def show_products(request):
+    products = Product.objects.all()
+    return render(request, "moderator/products.html", {'products': products})
+
+
+def show_product_delete(request, product_id):
+    return render(request, "moderator/product_delete.html", {'product_id': product_id})
 
 
 def show_users(request):
-    return render(request, "moderator/users.html")
+    users = User.objects.all()
+    return render(request, "moderator/users.html", {'users': users})
+
+
+def show_user_delete(request, user_id):
+    return render(request, "moderator/user_delete.html", {'user_id': user_id})
+
+
+def show_producers(request):
+    producers = Producer.objects.all()
+    return render(request, "moderator/producers.html", {'producers': producers})
+
+
+def show_producer_delete(request, producer_id):
+    return render(request, "moderator/producer_delete.html", {'producer_id': producer_id})
