476bd2b916639077cb9280d919de0cbff63fda8a
Daniel <daniel.byshovets@gmail.com>
2025-02-06 20:23:15 -0800

    Commit 13 (developed delete and detail view functions for moderator, fixed some errors)

--- main/moderator/urls.py
+++ main/moderator/urls.py
@@ -3,10 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.show_index, name="show_index"),
-    path('products', views.show_products, name="show_products"),
-    path('product_delete/<int:product_id>', views.show_product_delete, name="product_delete"),
-    path('users', views.show_users, name="show_users"),
-    path('user_delete/<int:user_id>', views.show_user_delete, name="user_delete"),
-    path('producers', views.show_producers, name="show_producers"),
-    path('producer_delete/<int:producer_id>', views.show_producer_delete, name="producer_delete")
+    path('products/', views.show_products, name="show_products"),
+    path('products/<int:product_id>/detail', views.show_product_detail, name="product_detail"),
+    path('products/<int:pk>/delete', views.ProductDeleteView.as_view(), name="product_delete"),
+    path('users/', views.show_users, name="show_users"),
+    path('users/<int:user_id>/detail', views.show_user_detail, name="user_detail"),
+    path('users/<int:pk>/delete', views.UserDeleteView.as_view(), name="user_delete"),
+    path('producers/', views.show_producers, name="show_producers"),
+    path('producers/<int:producer_id>/detail', views.show_producer_detail, name="producer_detail"),
+    path('producers/<int:pk>/delete', views.ProducerDeleteView.as_view(), name="producer_delete")
 ]
--- main/moderator/views.py
+++ main/moderator/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from user.models import User, Product, Producer
+from django.views.generic import DeleteView
 
 # Create your views here.
+
+
+class UserDeleteView(DeleteView):
+    model = User
+    success_url = '..'
+    template_name = 'moderator/user_delete.html'
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = '..'
+    template_name = 'moderator/product_delete.html'
+
+
+class ProducerDeleteView(DeleteView):
+    model = Producer
+    success_url = '..'
+    template_name = 'moderator/producer_delete.html'
 
 
 def show_index(request):
@@ -13,8 +32,9 @@ def show_products(request):
     return render(request, "moderator/products.html", {'products': products})
 
 
-def show_product_delete(request, product_id):
-    return render(request, "moderator/product_delete.html", {'product_id': product_id})
+def show_product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, "moderator/product_detail_view.html", {'product': product})
 
 
 def show_users(request):
@@ -22,8 +42,9 @@ def show_users(request):
     return render(request, "moderator/users.html", {'users': users})
 
 
-def show_user_delete(request, user_id):
-    return render(request, "moderator/user_delete.html", {'user_id': user_id})
+def show_user_detail(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return render(request, "moderator/user_detail_view.html", {'user': user})
 
 
 def show_producers(request):
@@ -31,5 +52,6 @@ def show_producers(request):
     return render(request, "moderator/producers.html", {'producers': producers})
 
 
-def show_producer_delete(request, producer_id):
-    return render(request, "moderator/producer_delete.html", {'producer_id': producer_id})
+def show_producer_detail(request, producer_id):
+    producer = get_object_or_404(Producer, id=producer_id)
+    return render(request, "moderator/producer_detail_view.html", {'producer': producer})
